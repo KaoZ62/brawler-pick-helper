aaafe74aee3dc04c9b6bf66cc1deca16f2e0ea2e
david camaj <davidcamaj@MacBook-Air.local>
2025-05-24 13:57:12 +0200

    update

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,4 @@
-""import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import brawlerIcons from "../brawlerIcons.ts";
 import BrawlerCard from "./components/BrawlerCard";
 
